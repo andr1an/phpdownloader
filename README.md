6c61546b2d8f79b3d6fd58fe4b02049c46d0d4a8
sandrianov <sandrianov@team.amocrm.ru>
2016-09-15 14:11:03 +0300

    Second version

--- README.md
+++ README.md
@@ -1,8 +1,24 @@
 # phpdownloader
-Automatic PHP tarball downloader written in Python 2.7
 
-* Parses php.net and gets all mirrors for latest PHP 7 tarball.
+Automatic PHP tarball downloader written in Python 2.7. I personally use this as a part
+of automatic Debian package build system.
+
+* Parses php.net and gets all mirrors for latest PHP tar.bz2-archive.
+* Verifies checksum before downloading, skips it if SHA256 is okay
+* Verifies checksum after download
 * Uses Python [multiprocessing](https://docs.python.org/2/library/multiprocessing.html) module
 for parallel pinging of mirrors and uses fastest mirror to download tarball.
-* Verifies checksum before download, skips if OK; also verifies checksum after download
-* Prints downloaded file path, else exits with code 1
+* Prints downloaded file path or exits error code
+
+## Usage
+
+    usage: phpdownloader.py [-h] [-r {new,stable,oldstable}] [-C DIRECTORY]
+    
+    optional arguments:
+      -h, --help	help message
+    
+      -r, --release {new,stable,oldstable}
+			Choose PHP release branch (were 7.0, 5.6 and 5.5 on 2016-09-15)
+    
+      -C, --directory DIRECTORY
+			Target directory
--- phpdownloader.py
+++ phpdownloader.py
@@ -1,39 +1,62 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2.7
 import os
 import sys
 import contextlib
+import argparse
 import urllib2
 from lxml import html
 import hashlib
-
-import multiping
-
 import json
+import multiping
 
 
 BASE_URL = 'http://php.net'
-DOWNLOAD_PATH = '/tmp'
 
 
-def parse_downloads_page(downloads_url):
+def parse_args(args):
+    p = argparse.ArgumentParser()
+
+    p.add_argument('-r', '--release',
+                   action='store',
+                   default='new',
+                   choices=['new', 'stable', 'oldstable'],
+                   help="PHP release branch")
+
+    p.add_argument('-C', '--directory',
+                   action='store',
+                   default='/opt',
+                   help="Target directory")
+
+    return p.parse_args(args)
+
+
+def parse_downloads_page(downloads_url, php_release='new'):
     """Parses data from php.net downloads page
     Returns:
         [php_mirrors_url, php_filename, php_hash]
     """
 
+    SKIP_INFO = {
+        'new': 0,
+        'stable': 1,
+        'oldstable': 2
+        }
+
+    skip_div = SKIP_INFO[php_release]
+
     XPATH_BASE = "//div[@class='content-box']/ul/li"
 
     with contextlib.closing(urllib2.urlopen(downloads_url)) as response:
         tree = html.fromstring(response.read())
-
         links = tree.xpath(XPATH_BASE + "/a")
-        if len(links) > 0:
-            php_mirrors_url = BASE_URL + links[0].attrib["href"]
-            php_filename = links[0].text_content()
-
         spans = tree.xpath(XPATH_BASE + "/span[@class='sha256']")
-        if len(spans) > 0:
-            php_hash = spans[0].text_content()
+
+        if len(links) > 0 and len(spans) > 0:
+            php_mirrors_url = BASE_URL + links[skip_div*7].attrib["href"]
+            php_filename    = links[skip_div*7].text_content()
+            php_hash        = spans[skip_div*3].text_content()
+        else:
+            raise Exception("Can't parse downloads page!")
 
     return [php_mirrors_url, php_filename, php_hash]
 
@@ -59,8 +82,9 @@ def parse_mirrors_page(mirrors_url):
         mirrors = tree.xpath("//div[@class='entry']")
         for m in mirrors:
             prov_name = m.xpath("div[@class='provider']/a")[0].text_content()
-            prov_url = m.xpath("div[@class='provider']/a")[0].attrib["href"]
-            php_url = m.xpath("div[@class='url']/a")[0].attrib["href"]
+            prov_url  = m.xpath("div[@class='provider']/a")[0].attrib["href"]
+            php_url   = m.xpath("div[@class='url']/a")[0].attrib["href"]
+
             mirrors_parsed.append({'name': prov_name,
                                    'provider': prov_url,
                                    'url': php_url})
@@ -110,42 +134,48 @@ def verify_php_hash(php_filename, php_hash):
 
 
 def mirror_speed_key(mirror):
-    return mirror["ping"]
+    return mirror['ping']
 
 
-def run():
+def run(args):
+    args = parse_args(args)
+
+    print "Searching for lastest available '%s' PHP release..." % args.release
+
     php_mirrors_url, php_filename, php_hash = parse_downloads_page(BASE_URL +
-                                                            '/downloads.php')
-    mirrors_parsed = parse_mirrors_page(php_mirrors_url)
+                                                '/downloads.php', args.release)
 
-    print "Got mirror list..."
-
-    mirrors_pinged = multiping.get_mirrors_pinged(mirrors_parsed, processes=16)
-    print "Pinged!"
-    mirrors_pinged.sort(key=mirror_speed_key)
-    print "Sorted!"
-    selected_mirror = mirrors_pinged[0]
-    # print "Mirror list:"
-    # print json.dumps(mirrors_pinged, indent=2)
-    print "Selected mirror:"
-    print json.dumps(selected_mirror, indent=2)
-
-    php_filename = os.path.join(DOWNLOAD_PATH, php_filename)
-
-    if not selected_mirror['url']:
-        print "Can't download from mirror: \"%s\"!" % selected_mirror['name']
-        return 1
+    php_filename = os.path.join(args.directory, php_filename)
 
     if os.path.isfile(php_filename):
         print "%s already exists!" % php_filename
         if verify_php_hash(php_filename, php_hash):
-            print os.path.join(php_filename)
+            print php_filename
             return 0
         else:
             print 'Wrong checksum! Redownloading...'
             os.remove(php_filename)
     else:
         print "New release found: %s" % php_filename
+
+    mirrors_parsed = parse_mirrors_page(php_mirrors_url)
+
+    print "Got mirror list..."
+    # print "Mirror list:"
+    # print json.dumps(mirrors_parsed, indent=2)
+
+    mirrors_pinged = multiping.get_mirrors_pinged(mirrors_parsed, processes=16)
+    print "Pinged!"
+    mirrors_pinged.sort(key=mirror_speed_key)
+    print "Sorted!"
+    selected_mirror = mirrors_pinged[0]
+
+    if not selected_mirror['url']:
+        print "Can't download from mirror: \"%s\"!" % selected_mirror['name']
+        return 1
+
+    print "Selected mirror:"
+    print json.dumps(selected_mirror, indent=2)
 
     download_php(selected_mirror['url'], php_filename)
 
@@ -157,5 +187,5 @@ def run():
 
 
 if __name__ == '__main__':
-    code = run()
+    code = run(sys.argv[1:])
     sys.exit(code)
